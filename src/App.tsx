9d122e60df86ee346bfea7ce8772013d2cca8544
Leonardo <leonardo.badilla@cobee.io>
2022-09-22 23:41:48 +0200

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const cards = ["joker", "card1", "card2", "card3", "card4", "card5"];
+
+  const [selectedCard, setSelectedCard] = useState<number>(0);
+
+  const selectRandomCard = () => {
+    const randomSelectedCard = Math.floor(Math.random() * 5);
+    setSelectedCard(randomSelectedCard);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>JOWI CARD GAME</div>
+      <div>{cards[selectedCard]}</div>
+      <button onClick={selectRandomCard}>
+        PRESIONA PARA DESTAPAR UN CARTA
+      </button>
     </div>
   );
 }
